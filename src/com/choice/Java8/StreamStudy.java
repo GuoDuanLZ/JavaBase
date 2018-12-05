d5da210f4e7d02b91b42f33cd3c2d28809883741
王正 <739537111@qq.com>
2018-12-05 16:20:48 +0800

    java8.stream并行

--- src/com/choice/Java8/StreamStudy.java
+++ src/com/choice/Java8/StreamStudy.java
@@ -1,10 +1,18 @@
 package com.choice.Java8;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class StreamStudy {
 
-    public void chap04(){
-
+    public static void streamMap(){
+        List<String> words = Arrays.asList("Java 8", "Lambdas", "In", "Action");
+        System.out.println(words.stream()
+                .collect(Collectors.toList()));
     }
 
-
+    public static void main(String[] args) {
+        streamMap();
+    }
 }
